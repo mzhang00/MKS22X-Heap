5f557ae27d472f6781ebbe63fe3d1fcd2279e436
Michael Zhang <mzhang00@stuy.edu>
2019-04-22 18:33:35 -0400

    Add HeapHelp to test Heaps

--- MyHeap.java
+++ MyHeap.java
@@ -1,9 +1,16 @@
 public class MyHeap{
     private static void pushDown(int[]data,int size,int index){
+        if (getChild(index) + 1 < size){
 
+        }
     }
     
     private static int getChild(int index){
         return 2 * index;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4};
+        System.out.println(HeapHelp.toString(arr));
     }
 }
