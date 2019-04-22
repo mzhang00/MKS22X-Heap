863325ef4a689dd6ac68f9411ee2b34da01a2e77
Michael Zhang <mzhang00@stuy.edu>
2019-04-22 17:30:29 -0400

    Add pushDown and helper methods

--- MyHeap.java
+++ MyHeap.java
@@ -1,3 +1,9 @@
 public class MyHeap{
-    //
+    private static void pushDown(int[]data,int size,int index){
+
+    }
+    
+    private static int getChild(int index){
+        return 2 * index;
+    }
 }
