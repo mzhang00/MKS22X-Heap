c63f195585d7744198347b757dd945b1db820cb2
Michael Zhang <mzhang00@stuy.edu>
2019-04-23 19:44:41 -0400

    Update pushUp

--- MyHeap.java
+++ MyHeap.java
@@ -25,8 +25,21 @@ public class MyHeap{
         }
     }
 
-    private static void pushUp(int[]data,int index){
-        //
+    private static void pushUp(int[]data, int index){
+        while (!(isRoot(index))){
+            if (data[index] < data[getParent(index)]){
+                return;
+            }else{
+                int temp = data[index];
+                data[index] = data[getParent(index)];
+                data[getParent(index)] = temp;
+                index = getParent(index);
+            }
+        }
+    }
+
+    private static boolean isRoot(int index){
+        return index == 0;
     }
 
     private static boolean isLeaf(int size, int index){
@@ -51,11 +64,25 @@ public class MyHeap{
         return (2 * index) + firstorsecond;
     }
 
+    private static int getParent(int index){
+        return (index - 1) / 2;
+    }
+
     public static void main(String[] args){
         int[] arr = {1,2,3,4,5};
         System.out.println(HeapHelp.toString(arr));
         System.out.println("--------------------------------------------");
         pushDown(arr, arr.length, 0);
+        System.out.println(HeapHelp.toString(arr));
+        System.out.println("--------------------------------------------");
+        for (int i : arr){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("--------------------------------------------");
+        System.out.println(getParent(2));
+        System.out.println("--------------------------------------------");
+        pushUp(arr, 4);
         System.out.println(HeapHelp.toString(arr));
     }
 }
